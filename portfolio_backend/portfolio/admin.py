1f87b0ca3facf675af724fb52062f76e05bd97b5
Ram Chandra <rcb.26498@gmail.com>
2023-07-01 14:02:21 -0400

    added portfolio backend

---
+++ portfolio_backend/portfolio/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Viewers, LikesModel, Feedbacks
+
+# Register your models here.
+
+admin.register(Viewers)
+admin.register(LikesModel)
+admin.register(Feedbacks)
