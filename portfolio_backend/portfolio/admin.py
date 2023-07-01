3e892443006c3e42e048b5d4a02334d9f399c75b
ram-1918 <109194855+ram-1918@users.noreply.github.com>
2023-07-01 14:10:39 -0400

    Delete portfolio_backend directory

--- portfolio_backend/portfolio/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Viewers, LikesModel, Feedbacks
-
-# Register your models here.
-
-admin.register(Viewers)
-admin.register(LikesModel)
-admin.register(Feedbacks)
