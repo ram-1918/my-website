408921b08e0fa6d7715bea63ac02e7114dc85b15
Ram Chandra <rcb.26498@gmail.com>
2023-07-05 15:44:46 -0400

    preeths feedback api endpoint-2

--- portfolio_backend/portfolio/services.py
+++ portfolio_backend/portfolio/services.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 def send_email_verification(email, recruiter, msg, sender='rcb.26498@gmail.com'):
     phone = '+17166171918' if sender == 'rcb.26498@gmail.com' else '+17168089656'
-    html_message = render_to_string('feedback.html', {"email": email.value, "sender":sender, "senderPH":phone}) if not recruiter else render_to_string('recruiter.html', {"email": email.value})
+    html_message = render_to_string('feedback.html', {"email": email.value, "sender":sender, "senderPH":phone}) if not recruiter else render_to_string('recruiter.html', {"email": email.value, "sender":sender})
     html_message1 = render_to_string('feedback1.html', {"email": email.value, "msg": msg})
     subject = 'Thanks for your feedback!'
     message = 'Thank you for the feedback!'
